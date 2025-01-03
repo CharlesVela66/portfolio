41c2525f649c17e29aeb7856fef4537588ab2a0a
Carlos <A01708634@tec.mx>
2025-01-02 20:35:43 -0600

    Fix responsive issues 2.0

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -54,7 +54,7 @@ const Hero = () => {
       </div>
       <SectionButton
         name="Let's work together!"
-        className="absolute bottom-7 left-0 right-0 w-full z-10 c-space"
+        className="absolute sm:bottom-7 bottom-20 left-0 right-0 w-full z-10 c-space"
         containerClassName="sm:w-fit w-full sm:min-w-96"
         href="#about"
       />
